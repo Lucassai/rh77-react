78e7835d85f51e2059464862330fd1281da30053
Lucas Oliveira <lucasvmo42@gmail.com>
2025-01-29 12:40:12 -0300

    Logica lista

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,18 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import ListarCargos from './components/cargos/listacargos/ListarCargos'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ListarCargos from "./components/cargos/listacargos/ListarCargos";
 import "./App.css";
-
-
 function App() {
   return (
     <>
-        <BrowserRouter>
-          <div className="min-h-[80vh]">
-            <Routes>
-              <Route path="/cargo" element={<ListarCargos />} />
-            </Routes>
-            
-          </div>
-        </BrowserRouter>
-      
+      <BrowserRouter>
+        <div className="min-h-[80vh]">
+          <Routes>
+            <Route path="/cargo" element={<ListarCargos />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </>
-
-  )
+  );
 }
 
-export default App
+export default App;
