f7e2dd832733872d248104ec9c8259d5b13d880a
Ninaraquel <nynaferreira.souza@gmail.com>
2025-01-29 12:22:51 -0300

    lista de cargos

---
+++ src/components/cargos/listacargos/ListarCargos.tsx
@@ -0,0 +1,19 @@
+import CardCargos from "../cardcargos/CardCargos";
+
+
+function ListarCargos() {
+
+    return (
+        <>
+            <div className="flex justify-center w-full my-4">
+                <div className="container flex flex-col">
+                    <div className="grid grid-cols-1 md:grid-cols-2 
+                                    lg:grid-cols-3 gap-8">
+                            <CardCargos />
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+export default ListarCargos;
