ad6155c3709b2b7d5cb4eead743140e371058108
Ivan Barbosa <ivanjulio.ij@gmail.com>
2025-01-29 11:49:25 -0300

    apptsx e service: As rotas do service estão prontas, o apptsx precisa das páginas e componentes para que as importações sejam feitas.

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,17 @@ import './App.css'
 import Footer from './components/footer/Footer'
 import Navbar from './components/navbar/NavBar'
 //import de cargos
-//impor de funcionarios
+import Funcionario from './models/Funcionario'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<Funcionario />} />
-
-      </Routes>
-      <Footer />
+      <Navbar/>
+        <Routes>
+          <Route path='/' element={<Funcionario />} />
+        </Routes>
+      <Footer/>
     </BrowserRouter>
   )
 }
